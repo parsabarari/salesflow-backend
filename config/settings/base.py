9a36921efbc1a627cfe575692bd1c65378fd6008
Parsa <parsaperix@gmail.com>
2026-07-15 04:44:08 +0330

    changed swagger theme to dark
    -started to change but it's not complete yet.

--- config/settings/base.py
+++ config/settings/base.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -54,6 +54,12 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
