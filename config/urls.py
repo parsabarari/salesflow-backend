c47e6cdc4a796df7173d23508df5ac5373d6bd23
Parsa <parsaperix@gmail.com>
2026-07-19 05:51:33 +0330

    scalar api documentation first setup

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (SpectacularAPIView,SpectacularSwaggerView,
                                    SpectacularRedocView,)
+from scalar.scalar import urlpatterns_scalar
 
 
 urlpatterns = [
@@ -14,4 +15,4 @@ urlpatterns = [
     path("api/schema/",SpectacularAPIView.as_view(),name="schema",),
     path("api/schema/swagger/",SpectacularSwaggerView.as_view(url_name="schema"),name="swagger-ui",),
     path("api/schema/redoc/",SpectacularRedocView.as_view(url_name="schema"),name="redoc",),
-]
+] + urlpatterns_scalar
